4c464f4dc6582a7305ee30a256f8c13af18bce8a
ravins <ravin.singh1990@gmail.com>
2015-09-28 11:55:15 +0530

    update

--- install.sh
+++ install.sh
@@ -1,15 +1,19 @@
 #!/bin/bash
 #This file is to create symlink for dotfiles in home directory
-dir=~/sublime-text-3-settings
-old_dir=~/sublime-text-setting-backup
-symlinkPath = ~/.config/sublime-text-3/Packages/User/
-echo "creating $old_dir folder for backup"
-mkdir -p $old_dir
+SAVEIFS=$IFS
+IFS=$(echo -en "\n\b")
+repositoryFolder=~/sublime-text-3-settings
+backup_folder=~/sublime-text-setting-backup
+symlinkPath=~/.config/sublime-text-3/Packages/User/
+echo "creating $backup_folder folder for backup"
+mkdir -p $backup_folder
 echo "......done"
-files="Package Control.sublime-settings Preferences.sublime-settings goto_css_declaration.sublime-settings"
+files="Package Control.sublime-settings
+Preferences.sublime-settings
+goto_css_declaration.sublime-settings"
 
-echo "changing to $dir folder"
-cd $dir
+echo "changing to $repositoryFolder folder"
+cd $repositoryFolder
 echo ".......done"
 
 #move any dotfile to backup and dotfiles folder
@@ -17,11 +21,12 @@ for file in $files
 	do
 		#check if file exists otherwise create blank one
 		if [ ! -f $symlinkPath ];then
-			echo "creating ~/.$file file"
-			touch ~/.$file
+			echo "creating $file file"
+			touch $symlinkPath/$file
 		fi
-		echo "moving $file in $old_dir"
-		mv $symlinkPath $old_dir
+		echo "moving $file in $backup_folder"
+		mv $symlinkPath/$file $backup_folder
 		echo "creating symlink"
-		ln -s $dir/$file $symlinkPath
+		ln -s $repositoryFolder/$file $symlinkPath
 	done
+IFS=$SAVEIFS
